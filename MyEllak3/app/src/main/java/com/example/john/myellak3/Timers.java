9ae2a9221c07db1ee930c3cd814714677ab050a9
johnskap <johnskap2004@yahoo.gr>
2015-08-02 12:28:13 +0300

    2

---
+++ MyEllak3/app/src/main/java/com/example/john/myellak3/Timers.java
@@ -0,0 +1,99 @@
+package com.example.john.myellak3;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class Timers extends Activity implements OnClickListener
+{
+    int Counter = 0;
+    Button BtStart;
+    Button BtStop;
+    Button BtReset;
+    TextView TvShow;
+    Timer MyTimer = null;
+    TimerTask MyTask = null;
+
+    @Override
+    protected void onCreate (Bundle savedInstanceState)
+    {
+        super.onCreate (savedInstanceState);
+        setContentView (R.layout.lay_main2);
+        BtStart = (Button) findViewById (R.id.BtStart);
+        BtStop = (Button) findViewById (R.id.BtStop);
+        BtReset = (Button) findViewById (R.id.BtReset);
+        TvShow = (TextView) findViewById (R.id.TvShow);
+        BtStart.setOnClickListener (this);
+        BtStop.setOnClickListener (this);
+        BtReset.setOnClickListener (this);
+    }
+
+    @Override
+    public void onClick (View v)
+    {
+        if (v == BtReset)
+        {
+            Counter = 0;
+            TvShow.setText ("0");
+        }
+        if (v == BtStart)
+            DoStart ();
+        if (v == BtStop)
+            DoStop ();
+    }
+
+    public void DoStart ()
+    {
+        MyTask = new TimerTask ()
+        {
+            public void run ()
+            {
+                Counter++;
+                ShowMessage (Counter);
+            }
+        };
+        if (MyTimer == null)
+            MyTimer = new Timer ();
+        MyTimer.schedule (MyTask, 0, 2000);
+    }
+
+    public void DoStop ()
+    {
+        if (MyTimer != null)
+        {
+            MyTimer.cancel ();
+            MyTimer.purge ();
+            MyTimer = null;
+        }
+    }
+
+    public void ShowMessage (int Val)
+    {
+        Message msg = new Message ();
+        Bundle bun = new Bundle ();
+        bun.putInt ("Value", Val);
+        msg.setData (bun);
+        MyHandler.sendMessage (msg);
+    }
+
+    Handler MyHandler = new Handler ()
+    {
+        @Override
+        public void handleMessage (Message Mess)
+        {
+            Bundle b = Mess.getData ();
+            int tbp = b.getInt ("Value");
+            TvShow.setText (String.valueOf (tbp));
+        }
+    };
+
+}
